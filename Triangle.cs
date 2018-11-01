8ce1b747c41cf675c9f42fed5082dac7c58d741e
Matthew Geppert <mdgeppert@gmail.com>
2018-11-01 12:46:21 -0500

    Add files via upload

---
+++ Triangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace TriangleApplication
+{
+	class Triangle
+	{
+		double leftSide;
+		double bottomSide;
+		public void AcceptSides()
+		{
+			leftSide = 3;
+			bottomSide = 4;
+			
+		}
+		public double GetArea()
+		{
+			return .5 * ( bottomSide * leftSide);
+		}
+
+		public double GetHypotenuse()
+		{
+			double Hypnse = Math.Pow(leftSide, 2) + Math.Pow(bottomSide, 2);
+			return Math.Sqrt((double) Hypnse);
+		}
+		public void Display()
+		{
+			Console.WriteLine("The left side is {0} units long.", leftSide);
+			Console.WriteLine("The bottom side is {0} units long.", bottomSide);
+			Console.WriteLine("The hypotenuse is {0} units long", GetHypotenuse());
+			Console.WriteLine("The area of the triangle is {0} square units.", GetArea());
+
+		}
+		class ExecuteTriangle
+		{
+			static void Main(string[] args)
+			{
+				Triangle t = new Triangle();
+				t.AcceptSides();
+				t.GetArea();
+				t.Display();
+				t.GetArea();
+				t.GetHypotenuse();
+				Console.WriteLine("Press Enter to continue.");
+				Console.ReadLine();
+
+
+			}
+		}
+
+		
+	}
+
+}
+
